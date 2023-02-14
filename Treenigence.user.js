5b4e8748614fc3450738fdd4d7930dd05cbf4564
TripleCamera <TripleCamera@outlook.com>
2023-02-14 19:09:09 +0800

    Add files via upload

---
+++ Treenigence.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Treenigence
+// @namespace    Treenigence
+// @version      0.1
+// @description  Enhancement for Treenity(智慧树).
+// @author       TripleCamera2022
+// @match        https://ainew.zhihuishu.com/*
+// @grant        none
+// @run-at       document-end
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert('Hello World!')
+})();
