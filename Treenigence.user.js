0c3b832e4cb9997fe05e71d6a8f466a122a52749
TripleCamera <TripleCamera@outlook.com>
2023-05-09 20:33:26 +0800

    知识点练习报告 - 截图（测试）、自动关闭提示

--- Treenigence.user.js
+++ Treenigence.user.js
@@ -1,18 +1,33 @@
 // ==UserScript==
 // @name         Treenigence
 // @namespace    Treenigence
-// @version      0.1
+// @version      0.2
 // @description  Enhancement for Treenity(智慧树).
 // @author       TripleCamera2022, 14725
 // @match        https://ainew.zhihuishu.com/*
 // @grant        none
 // @run-at       document-end
 // @license      MIT
+// @require      https://html2canvas.hertzen.com/dist/html2canvas.min.js
+// @require      https://unpkg.com/html-to-image@1.11.11/dist/html-to-image.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     debugger;
+
+    window.setTimeout(knowledgeAnalysis, 5000);
+})();
+
+function injectCSS(text)
+{
+    var styleEle = document.createElement("style");
+    styleEle.innerHTML = text;
+    document.documentElement.appendChild(styleEle);
+}
+
+function knowledgeAnalysis()
+{
     injectCSS(`
         .wrongLIST,
         .currentLIST {
@@ -39,11 +54,21 @@
             height: auto !important;
         }
     `);
-})();
 
-function injectCSS(text)
-{
-    var styleEle = document.createElement("style");
-    styleEle.innerHTML = text;
-    document.documentElement.appendChild(styleEle);
+    // html2canvas(document.querySelector(".listTi")).then(canvas => {
+    //     var link = document.createElement("a");
+    //     link.download = 'test';
+    //     link.href = canvas.toDataURL();
+    //     link.click();
+    // });
+
+    htmlToImage.toPng(document.querySelector(".listTi")).then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
+
+    // 自动关闭提示
+    document.querySelector('.Tips .ZHIHUISHU_QZMD').click();
 }
